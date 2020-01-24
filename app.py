6281e4d59e9447baa74180cf948edbd1cb887e10
Christofer Bertonha <christofer.bertonha@typeform.com>
2020-01-24 11:01:11 +0100

    add Health entrypoint

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ def hello_world():
     return 'Hello World!'
 
 
+@app.route('/health')
+def health():
+    return 'System running'
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=8000, debug=True)
